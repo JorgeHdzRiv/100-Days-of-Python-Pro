59ac609ec6a64c7175a9e701aa0f86e87e1e1824
Jorge Hdz <jor.hdz.riv@gmail.com>
2025-02-13 15:26:15 -0600

    Pr Day1

---
+++ p1_band_name/main.py
@@ -0,0 +1,19 @@
+"""
+Instruciones para ejecutar el programa:
+1. Crea un saludo para tu programa.
+2. Pide al usuario la ciudad en la que creció y almacénala en una variable.
+3. Pide al usuario el nombre de una mascota y almacénalo en una variable.
+4. Combina el nombre de su ciudad y mascota y muéstrales el nombre de su banda.
+"""
+
+# 1. Crear un saludo para el programa
+print("¡Bienvenido a la generadora de nombres de bandas!")
+
+# 2. Pedir al usuario la ciudad en la que creció y almacenarla en una variable
+ciudad = input("¿En qué ciudad creciste?:\n")
+
+# 3. Pedir al usuario el nombre de una mascota y almacenarlo en una variable
+mascota = input("¿Cuál es el nombre de tu mascota?:\n")
+
+# 4. Combinar el nombre de la ciudad y la mascota y mostrar el nombre de la banda
+print("¡Tu nombre de banda podría ser " + ciudad + " " + mascota + "!")
